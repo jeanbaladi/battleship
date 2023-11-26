5985d8e59f469354fa6d47645da2aa18207b2b85
Jean Paul Baladi <yanbaladi159@gmail.com>
2023-11-26 13:52:13 -0300

    root routes created

--- src/app/root/root-routing.module.ts
+++ src/app/root/root-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthModule } from '../views/auth/auth.module';
-
-
 const routes: Routes = [
   {path:"" , loadChildren : () => import('../views/auth/auth.module').then(m => m.AuthModule)},
-  
+  {path:"lobby" , loadChildren : () => import('../views/lobby/lobby.module').then(m => m.LobbyModule)},
+  {path:"inGame:idGame" , loadChildren : () => import('../views/in-game/in-game.module').then(m => m.InGameModule)},
 ];
 
 @NgModule({
