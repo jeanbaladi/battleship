0359073779625cacc93853487ac416e660650f74
Jean Paul Baladi <yanbaladi159@gmail.com>
2024-03-01 16:41:23 -0300

    fix bug

--- src/app/shared/nav-bar/nav-bar-component/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar-component/nav-bar.component.ts
@@ -25,6 +25,8 @@ export class NavBarComponent implements OnInit {
       if(profilePath){
         profilePath.isAccessible = false;
       }
+    }else{
+      this.routes.forEach(r => r.isAccessible = true);
     }
   }
 
